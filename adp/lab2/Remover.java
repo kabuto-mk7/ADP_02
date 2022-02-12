0ff2120e95fb5250cef56b9b607b38866b34316f
Kemal Mehmet <73295078+kabuto-mk7@users.noreply.github.com>
2022-02-12 18:41:08 +0000

    Added main files

--- adp/lab2/Remover.java
+++ adp/lab2/Remover.java
@@ -5,14 +5,21 @@ import java.util.List;
 class Remover implements Runnable {
     private final List<String> list;
     public Remover( final List<String> list) {
-        this.list = list;
+        synchronized(this) {
+            this.list = list;
+        }
     }
     public void run() {
-        while( this.list.size() > 0) {
+        while (this.list.size() > 0) {
             try {
-                Thread.sleep( (long)(Math.random() * 100));
-            } catch( final InterruptedException e) {}
-            System.out.println( "Removing " + this.list.remove( this.list.size() - 1));
+                Thread.sleep((long) (Math.random() * 100));
+            } catch (final InterruptedException e) {
+            }
+            synchronized(this.list) {
+                if (list.size() > 0) {
+                    System.out.println("Removing " + this.list.remove(this.list.size() - 1));
+                }
+            }
         }
     }
 }
--- adp/lab2/ShowLast.java
+++ adp/lab2/ShowLast.java
@@ -4,18 +4,28 @@ import java.util.List;
 
 class ShowLast implements Runnable {
     private final List<String> list;
-    public ShowLast( final List<String> list) {
-        this.list = list;
+
+    public ShowLast(final List<String> list) {
+        synchronized (this) {
+            this.list = list;
+        }
     }
+
     public void run() {
-        while( this.list.size() > 0) {
-            int lastItem = this.list.size() - 1;
-            Thread.yield();
-            System.out.println( "Last item is " +
-                    this.list.get( lastItem));
+        synchronized (this.list) {
+            while (true) {
+                if (list.size() > 0) {
+                    int lastItem = this.list.size() - 1;
+                    Thread.yield();
+                    System.out.println("Last item is " + this.list.get(lastItem));
+                } else {
+                    break;
+                }
+            }
             try {
-                Thread.sleep( (long)(Math.random() * 100));
-            } catch( final InterruptedException e) {}
+                Thread.sleep((long) (Math.random() * 100));
+            } catch (final InterruptedException e) {
+            }
         }
     }
 }
